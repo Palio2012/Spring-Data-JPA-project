ecb842d8a4ed8654abe338752e7c113dfb89a19d
Palio2012 <daniasrayan.com@gmail.com>
2022-11-15 18:15:04 -0300

    Project Created

---
+++ src/main/java/io/github/palio2012/localizacao/LocalizacaoApplication.java
@@ -0,0 +1,13 @@
+package io.github.palio2012.localizacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalizacaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalizacaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/palio2012/localizacao/LocalizacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.palio2012.localizacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalizacaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
